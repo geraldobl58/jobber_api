f2db641b89fed0d9b319ce2d365589ec14ed7d33
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-31 17:40:19 -0300

    feat: implement login mutation in AuthResolver with context handling

--- apps/jobber-auth/src/app/auth/auth.resolver.ts
+++ apps/jobber-auth/src/app/auth/auth.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { GqlContext } from '@jobber-api/nestjs';
+
+import { User } from '../users/models/user.model';
+import { LoginInput } from './dto/login-input';
+import { AuthService } from './auth.service';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => User)
+  async login(
+    @Args('loginInput') loginInput: LoginInput,
+    @Context() context: GqlContext
+  ) {
+    return this.authService.login(loginInput, context.res);
+  }
+}
