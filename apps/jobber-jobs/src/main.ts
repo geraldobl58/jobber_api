c80e383a6e683b51df40738d3a048d5b3be61c1a
Geraldo Luiz <geraldobl58@gmail.com>
2025-04-01 09:52:14 -0300

    feat: implement main entry point for jobber-jobs application

---
+++ apps/jobber-jobs/src/main.ts
@@ -0,0 +1,28 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const globalPrefix = 'api';
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix(globalPrefix);
+
+  const port = app.get(ConfigService).getOrThrow('PORT') || 3001;
+
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
